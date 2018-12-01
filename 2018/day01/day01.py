0d168c9fcce87e21ec2126cc4cd3534b89df9868
Volker Weber <volker@luppis.de>
2018-12-01 20:48:45 +0100

    - added day1

--- 2018/day01/day01.py
+++ 2018/day01/day01.py
@@ -1,4 +1,22 @@
-
-
+#! /usr/bin/python
 
 print("Hello 2018, Day1")
+
+data = open('input01.txt', 'r').read().splitlines()
+
+len = len(data)
+
+list = []
+
+print("Len: " + str(len))
+
+sum = 0
+
+list.append(sum)
+
+for x in range(0, 300):
+    for val in data:
+        sum = sum + int(val)
+        if(sum in list):
+            print("Found something!: " + str(sum))
+        list.append(sum)
